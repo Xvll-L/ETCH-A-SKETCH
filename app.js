e549b437937cbd0e624c24b9a545145f73cf7157
Ahmed <ahmedmo501296@gmail.com>
2022-07-05 13:30:22 +0100

    generates square boxes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const esketch = document.querySelector(".eSketchBox")
 test.innerHTML = "";
 
-
+// 16 x 16 grid test
 function test(){
-    let i =  0;
-    while(i < 20){
-        const test = document.createElement('div');
-        //test.textContent = `${i}`
-        esketch.appendChild(test)
-        i= i + 1;
-        console.log(i);
+    
+        let input = prompt("type number from 1 to 64")
+        let num = parseInt(input)
+        console.log(num)
+        esketch.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+        esketch.style.gridRow = `repeat(${num}, 1fr)`;
+
+        let loop = num * num   
+        let i =  0;
+        while(i < loop){
+            const test = document.createElement('div');
+            //test.textContent = `${i}`
+            esketch.appendChild(test)
+            i= i + 1;
+            console.log(i);
+            esketch.style.margin = '10px'
     }
 }
 
